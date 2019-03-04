9423d8311486cb485bc18e36cc810ab54c5b06b4
310158964 <310158964@NLYBSTQVP4NB88V>
2019-03-04 14:56:40 +0100

    adding duplicate entries file

---
+++ DuplicateEntries.py
@@ -0,0 +1,14 @@
+import csv
+
+uniqueList = []
+duplicateList = []
+with open('C:\Views\Trunk\Build_\ApprovedBinaries.txt', 'r') as csvFile:
+    reader = csv.reader(csvFile)
+    for row in reader:
+        if row is not []:
+            if  row[0].startswith("#") or row not in uniqueList:
+                uniqueList.append(row)
+            else:
+                duplicateList.append(row)
+
+csvFile.close()
